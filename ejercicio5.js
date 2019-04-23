d6bf641ad2176c68922f1d67057ce61a2761d28a
Eduardo Straub <eduardo.straub1@gmail.com>
2019-04-22 21:10:33 -0400

    Se agregan titulos a los ejercicios y se agrega un js

---
+++ ejercicio5.js
@@ -0,0 +1,5 @@
+var result = document.getElementsByName("result")[0];
+var calculator = document.getElementsByName("calculator")[0];
+var calcTotal = function(){
+  calculator.value = eval(calculator.value);
+}
