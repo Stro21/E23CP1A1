307359e4fe5068699236ddc2966d587af6f219e2
Eduardo Straub <eduardo.straub1@gmail.com>
2019-04-22 21:51:57 -0400

    Se completo el ejercicio 5

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,5 +1,11 @@
 var result = document.getElementsByName("result")[0];
 var calculator = document.getElementsByName("calculator")[0];
+
 var calcTotal = function(){
   calculator.value = eval(calculator.value);
+}
+
+
+function number(n) {
+  calculator.value += n
 }
